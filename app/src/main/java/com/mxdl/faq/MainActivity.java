e89a9c778a845b3f6eb7b89936d290e9621027e6
geduo83 <geduo_83@163.com>
2019-08-19 10:06:52 +0800

    init commit

---
+++ app/src/main/java/com/mxdl/faq/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mxdl.faq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Description: <MainActivity><br>
+ * Author:      mxdl<br>
+ * Date:        2019/8/19<br>
+ * Version:     V1.0.0<br>
+ * Update:     <br>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
