d45720f640407d30f19fd08812aa51d817b40df5
geduo83 <geduo_83@163.com>
2019-09-30 12:33:53 +0800

    code perfect

---
+++ app/src/main/java/com/mxdl/faq/view/HorizontalScrollView.java
@@ -0,0 +1,11 @@
+package com.mxdl.faq.view;
+
+/**
+ * Description: <HorizontalScrollView><br>
+ * Author:      mxdl<br>
+ * Date:        2019/9/30<br>
+ * Version:     V1.0.0<br>
+ * Update:     <br>
+ */
+public class HorizontalScrollView {
+}
